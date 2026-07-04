c648b3beb6571a2f050353c60ab2dc2294720315
Brandon Ly <brandonly@lostcoding.com>
2026-07-03 22:17:18 -0500

    finish first c exercise

--- c/exercises/ex01_bytes.c
+++ c/exercises/ex01_bytes.c
@@ -1,14 +1,34 @@
 #include "ex01_bytes.h"
 
 void reverse_bytes(uint8_t *buf, size_t len) {
-    (void)buf;
-    (void)len;
-    /* TODO: two pointers from each end, swap until they meet */
+
+  if (len <= 1) {
+    return;
+  }
+
+  /* TODO: two pointers from each end, swap until they meet */
+  int left = 0;
+  int right = len - 1;
+  while (left != right) {
+    // Get the element on the left
+    uint8_t left_byte = buf[left];
+    // Store the element on the right. Probably necessary to store both, but
+    // whatever.
+    uint8_t right_byte = buf[right];
+
+    // swap!
+    buf[left] = right_byte;
+    buf[right] = left_byte;
+
+    left += 1;
+    right -= 1;
+  }
 }
 
 uint16_t sum_bytes(const uint8_t *buf, size_t len) {
-    (void)buf;
-    (void)len;
-    /* TODO: walk the buffer and accumulate */
-    return 0;
+  uint8_t total = 0;
+  for (size_t i = 0; i < len; i++) {
+    total += buf[i];
+  }
+  return total;
 }
