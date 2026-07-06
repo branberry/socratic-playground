6fcfb316ef5df75c7b2929476773e344d77957f2
Brandon Ly <brandonly@lostcoding.com>
2026-07-05 20:12:47 -0500

    add basic file handling

--- c/exercises/ex03_file.c
+++ c/exercises/ex03_file.c
@@ -4,5 +4,11 @@
 int read_file(const char *path, uint8_t *buf, size_t cap, size_t *out_len) {
   /* TODO: fopen, fread or fgetc loop, fclose — check errors */
   FILE *fp = fopen(path, "rb");
-  return -1;
+
+  if (fp == NULL) {
+    return -1;
+  }
+
+  size_t bytes_read = fread(buf, 1, cap, fp);
+  return 0;
 }
