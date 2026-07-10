823af918c9ff5408c2a663a2dcd7bc910807cf11
Brandon Ly <brandonly@lostcoding.com>
2026-07-09 21:29:27 -0500

    add store

--- rust-rag-learn/src/lib.rs
+++ rust-rag-learn/src/lib.rs
@@ -7,6 +7,6 @@
 pub mod chunk;
 pub mod embed;
 pub mod rust_warmup;
-
+pub mod store;
 // TODO(step-1): Uncomment when you define these in chunk.rs:
 // pub use chunk::{Chunk, ChunkError, TextChunker};
