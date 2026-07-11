f8e29a7f54512f02b801edc079f1363027a71d0b
Brandon Ly <brandonly@lostcoding.com>
2026-07-10 21:04:37 -0500

    add StoredDocument struct

--- rust-rag-learn/src/store.rs
+++ rust-rag-learn/src/store.rs
@@ -1,2 +1,10 @@
-struct Document {}
-struct ScoredDocument {}
+use crate::chunk::Chunk;
+
+pub struct Document {
+    chunk: Chunk,
+    vector: Vec<f32>,
+}
+pub struct ScoredDocument {
+    document: Document,
+    pub score: f32,
+}
