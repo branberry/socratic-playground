7f6dd0ec4be518ac3fdbeca62a1d9b3724572d9b
Brandon Ly <brandonly@lostcoding.com>
2026-07-10 21:05:44 -0500

    add inmemoryvectorstore

--- rust-rag-learn/src/store.rs
+++ rust-rag-learn/src/store.rs
@@ -1,10 +1,14 @@
 use crate::chunk::Chunk;
 
 pub struct Document {
-    chunk: Chunk,
-    vector: Vec<f32>,
+    pub chunk: Chunk,
+    pub vector: Vec<f32>,
 }
 pub struct ScoredDocument {
-    document: Document,
+    pub document: Document,
     pub score: f32,
+}
+
+pub struct InMemoryVectorStore {
+    pub store: Vec<Document>,
 }
